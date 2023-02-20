3695a6218dc78113201e613315c5bab3f409d65d
alexgallardo11 <alexgallardo11@gmail.com>
2023-02-20 11:02:20 +0100

    MS-7: Fix crash when selecting Completed from task list

--- androidApp/src/main/java/co/softov/morestuff/android/ui/list/model/TaskListItemMapper.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/list/model/TaskListItemMapper.kt
@@ -1,8 +1,10 @@
 package co.softov.morestuff.android.ui.list.model
 
 import co.softov.morestuff.android.domain.model.Task
-import kotlinx.datetime.toLocalDateTime
 import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class TaskListItemMapper {
 
@@ -10,11 +12,22 @@ class TaskListItemMapper {
         SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.SHORT)
 
     fun map(input: Task): TaskListItemViewModel {
-        val createTime = input.createTime.toLocalDateTime().toString()
+        val inputDateTime = ZonedDateTime.now().toString()
+        val formatter = DateTimeFormatter.ISO_DATE_TIME.withZone(ZoneId.systemDefault())
+        val createTime = ZonedDateTime.parse(inputDateTime, formatter)
+            .withZoneSameInstant(ZoneId.systemDefault())
+            .toLocalDateTime()
+            .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
+
+
+
+        //val createTime = input.createTime.toLocalDateTime().toString()
         val completeTime = when (input.completeTime) {
             null -> ""
             else -> ""//input.completeTime.toLocalDateTime().toString()
         }
+
+
 
         return TaskListItemViewModel(
             id = input.id,
