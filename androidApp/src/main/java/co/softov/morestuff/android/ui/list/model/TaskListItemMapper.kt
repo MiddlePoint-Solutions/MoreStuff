bdb01873afc97a868f986e36e9913a90d0dea649
alexgallardo11 <alexgallardo11@gmail.com>
2023-02-20 14:50:13 +0100

    MS-11:Fix Time Bug when task is completed

--- androidApp/src/main/java/co/softov/morestuff/android/ui/list/model/TaskListItemMapper.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/list/model/TaskListItemMapper.kt
@@ -12,31 +12,46 @@ class TaskListItemMapper {
         SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.SHORT)
 
     fun map(input: Task): TaskListItemViewModel {
+        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault())
         val inputDateTime = ZonedDateTime.now().toString()
-        val formatter = DateTimeFormatter.ISO_DATE_TIME.withZone(ZoneId.systemDefault())
-        val createTime = ZonedDateTime.parse(inputDateTime, formatter)
-            .withZoneSameInstant(ZoneId.systemDefault())
-            .toLocalDateTime()
-            .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
 
-
-
-        //val createTime = input.createTime.toLocalDateTime().toString()
-        val completeTime = when (input.completeTime) {
-            null -> ""
-            else -> ""//input.completeTime.toLocalDateTime().toString()
+        val createTime: String
+        val completeTime: String? = if (input.completeTime != null) {
+            val completeTimeFormatted = markCompleteTime(input.completeTime)
+            val zonedDateTime = ZonedDateTime.parse(completeTimeFormatted, formatter)
+            createTime = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault())
+                .toLocalDateTime()
+                .format(formatter)
+            completeTimeFormatted
+        } else {
+            val zonedDateTime = ZonedDateTime.parse(inputDateTime, DateTimeFormatter.ISO_DATE_TIME.withZone(ZoneId.systemDefault()))
+            createTime = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault())
+                .toLocalDateTime()
+                .format(formatter)
+            null
         }
 
 
 
-        return TaskListItemViewModel(
+
+    return TaskListItemViewModel(
             id = input.id,
             createTime = createTime,
-            completeTime = completeTime,
+            completeTime = completeTime?:"",
             title = input.title
         )
     }
 
+    private fun markCompleteTime(timeString: String?): String? {
+        return timeString?.let {
+            val formatter = DateTimeFormatter.ISO_DATE_TIME.withZone(ZoneId.systemDefault())
+            val localDateTime = ZonedDateTime.parse(it, formatter)
+                .withZoneSameInstant(ZoneId.systemDefault())
+                .toLocalDateTime()
+            val timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+            localDateTime.format(timeFormatter)
+        }
+    }
     fun map(input: List<Task>): List<TaskListItemViewModel> {
         return input.map { map(it) }
     }
