48d845e274d4b39e31f558712a057c492deb6d4a
alexgallardo11 <alexgallardo11@gmail.com>
2023-02-20 11:11:10 +0100

    MS-8: Replace SharedPreferences with MultiPlatform Settings for app settings

--- androidApp/src/main/java/co/softov/morestuff/android/data/mapper/MessageDataMapper.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/data/mapper/MessageDataMapper.kt
@@ -3,6 +3,10 @@ package co.softov.morestuff.android.data.mapper
 import co.softov.morestuff.android.domain.enums.ContentType
 import co.softov.morestuff.android.domain.enums.ReplyType
 import co.softov.morestuff.android.domain.model.Message
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 typealias MessageData = co.softov.morestuff.db.Message
 typealias MessageDbMapper = (MessageData) -> Message
@@ -12,16 +16,42 @@ fun makeMessageDbMapper(): MessageDbMapper = { message ->
 }
 
 fun mapMessageDb(input: MessageData): Message {
+    val createTime = formatTime(input.create_time)
+    val seenTime = formatTime(input.seen_time)
+    val replyTime = formatTime(input.reply_time)
     return Message(
         id = input.id,
         taskId = input.task_id,
         scheduleId = input.schedule_id,
         contentType = ContentType.withValue(input.content_type),
-        createTime = input.create_time,
-        seenTime = input.seen_time,
+        createTime = createTime?: "",
+        seenTime = seenTime,
         content = input.content,
         replyType = input.reply_type?.let { ReplyType.withValue(it) },
         replyContent = input.reply_content,
-        replyTime = input.reply_time
+        replyTime = replyTime
     )
 }
+
+fun formatTime(timeString: String?): String? {
+    return timeString?.let {
+        val formatter = DateTimeFormatter.ISO_DATE_TIME.withZone(ZoneId.systemDefault())
+        val localDateTime = ZonedDateTime.parse(it, formatter)
+            .withZoneSameInstant(ZoneId.systemDefault())
+            .toLocalDateTime()
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+        localDateTime.format(timeFormatter)
+    }
+}
+
+
+//NEW FORM
+
+/*fun formatTime(timeString: String?): String? {
+    return timeString?.let {
+        LocalDateTime.parse(it, DateTimeFormatter.ISO_DATE_TIME)
+            .format(DateTimeFormatter.ofPattern("HH:mm"))
+    }
+}*/
+
+
