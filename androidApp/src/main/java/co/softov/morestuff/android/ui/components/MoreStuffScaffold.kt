882b7f003a9c40f1a9b4026d0c2a0ce2b8bab7c7
alexgallardo11 <alexgallardo11@gmail.com>
2023-03-21 10:56:10 +0100

    Settings should slide in from the right

--- androidApp/src/main/java/co/softov/morestuff/android/ui/components/MoreStuffScaffold.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/components/MoreStuffScaffold.kt
@@ -1,14 +1,25 @@
 package co.softov.morestuff.android.ui.components
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import co.softov.morestuff.android.ui.drawer.DrawerLayout
+import co.softov.morestuff.android.ui.settings.SettingsScreen
 import co.softov.morestuff.android.ui.theme.MoreStuffTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -20,8 +31,11 @@ fun MoreStuffScaffold(
 ) {
     val drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope: CoroutineScope = rememberCoroutineScope()
+    val settingsVisible = remember { mutableStateOf(false) }
 
-
+    BackHandler(enabled = settingsVisible.value) {
+        settingsVisible.value = false
+    }
 
     BackHandler(enabled = drawerState.isOpen) {
         if (drawerState.isOpen) {
@@ -38,7 +52,7 @@ fun MoreStuffScaffold(
             drawerContent = {
                 ModalDrawerSheet {
                     DrawerLayout {
-                        onSettingsClicked()
+                        settingsVisible.value = true
                         scope.launch {
                             drawerState.close()
                         }
@@ -59,5 +73,22 @@ fun MoreStuffScaffold(
                 )
             }
         )
+        @OptIn(ExperimentalAnimationApi::class)
+        AnimatedVisibility(
+            visible = settingsVisible.value,
+            enter = slideInHorizontally(initialOffsetX = { it }),
+            exit = slideOutHorizontally(targetOffsetX = { it })
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                SettingsScreen()
+
+            }
+        }
     }
 }
+
+
+
+
+
+
