c7d9bf7d2891b1efd146c37651ed63e48abfdaed
Joe3112 <jhsagiv@gmail.com>
2023-04-20 19:24:41 +0200

    Version bump -> 0.3.4(7)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         compileSdk = 33
         minSdk = 25
         targetSdk = 33
-        versionCode = 6
-        versionName = "0.3.3"
+        versionCode = 7
+        versionName = "0.3.4"
     }
 
     buildTypes {
