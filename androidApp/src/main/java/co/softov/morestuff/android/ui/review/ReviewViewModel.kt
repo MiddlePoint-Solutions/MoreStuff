6711a76f04e374af8ec6ad59deaffc81d969d91a
Joe3112 <jhsagiv@gmail.com>
2023-04-20 16:54:38 +0200

    MS-99: one step review (#24)
    * Use only one step for review
    
    * Fix broken test

---
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/review/ReviewViewModel.kt
@@ -0,0 +1,122 @@
+package co.softov.morestuff.android.ui.review
+
+import androidx.lifecycle.viewModelScope
+import co.softov.morestuff.android.app.presentation.viewmodel.BaseViewModel
+import co.softov.morestuff.android.domain.redux.state.ReviewAction
+import co.softov.morestuff.android.domain.usecase.priority.GetSchedulesForPriorityReviewUseCase
+import co.softov.morestuff.android.presentation.presenter.ReviewModel
+import co.softov.morestuff.android.presentation.presenter.ReviewViewEvent
+import co.softov.morestuff.android.presentation.presenter.ReviewViewEvent.*
+import co.softov.morestuff.android.presentation.presenter.ReviewRound
+import co.softov.morestuff.android.presentation.presenter.ReviewRound.*
+import co.softov.morestuff.android.ui.list.model.ScheduleListItemMapper
+import co.softov.morestuff.android.ui.list.model.ScheduleListItemViewModel
+import co.softov.morestuff.android.ui.review.swipeable.SwipeDirection
+import kotlinx.coroutines.launch
+
+class ReviewViewModel(
+    private val getSchedulesForPriorityReviewUseCase: GetSchedulesForPriorityReviewUseCase
+) : BaseViewModel<ReviewModel, ReviewViewEvent>(ReviewModel()) {
+
+    private val mapper = ScheduleListItemMapper()
+
+    override val enableDebug: Boolean
+        get() = true
+
+    init {
+        loadData()
+    }
+
+    override fun onLoadData() {
+        setInitialState()
+    }
+
+    override fun onReduceState(event: ReviewViewEvent) = when (event) {
+        is SetupInitialRound -> {
+            ReviewModel(
+                round = event.round,
+                roundNumber = 1,
+                roundItems = event.items
+            )
+        }
+
+        is SetupRound -> when (event.round) {
+            Priority -> {
+                ReviewModel(
+                    round = Priority,
+                    roundNumber = state.roundNumber + 1,
+                    roundItems = state.roundItems
+                )
+            }
+
+            Final -> {
+                state.copy(
+                    round = Final,
+                    roundNumber = state.roundNumber + 1,
+                    roundItems = state.high + state.low,
+                )
+            }
+        }
+
+        is OnHighPriority -> state.copy(high = state.high + event.item)
+        is OnLowPriority -> state.copy(low = state.low + event.item)
+        is OnDone -> state.copy(done = state.done + event.item)
+        is OnTomorrow -> state.copy(tomorrow = state.tomorrow + event.item)
+        is Undo -> state.copy(
+            high = state.high - event.item,
+            low = state.low - event.item,
+            done = state.done - event.item,
+            tomorrow = state.tomorrow - event.item,
+        )
+    }
+
+    private fun setInitialState(round: ReviewRound = Priority) {
+        viewModelScope.launch {
+            val schedules = getSchedulesForPriorityReviewUseCase().map(mapper::map).shuffled()
+            sendEvent(SetupInitialRound(round, schedules))
+        }
+    }
+
+    fun reset() {
+        setInitialState()
+    }
+
+    fun undoTask(schedule: ScheduleListItemViewModel) {
+        sendEvent(Undo(schedule))
+    }
+
+    fun confirmResults() {
+        dispatchAppStoreAction(
+            ReviewAction.ScheduleReviewResults(
+                tomorrow = state.tomorrow.map { it.taskId },
+                high = state.high.map { it.taskId },
+                low = state.low.map { it.taskId },
+                done = state.done.map { it.taskId },
+            )
+        )
+        navigateBack()
+    }
+
+    fun onTaskSwiped(
+        schedule: ScheduleListItemViewModel,
+        direction: SwipeDirection,
+        isLast: Boolean
+    ) {
+        val event = when (direction) {
+            SwipeDirection.Left -> OnLowPriority(schedule)
+            SwipeDirection.Right -> OnHighPriority(schedule)
+            SwipeDirection.Up -> OnDone(schedule)
+            SwipeDirection.Down -> OnTomorrow(schedule)
+        }
+        sendEvent(event)
+
+        if (isLast) {
+            sendEvent(SetupRound(Final))
+        }
+    }
+
+    companion object {
+        const val FINAL_ROUND_MINIMUM = 1
+    }
+
+}
