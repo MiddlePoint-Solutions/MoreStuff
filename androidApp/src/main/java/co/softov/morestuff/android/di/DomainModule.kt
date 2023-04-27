e06efe153e5997f5204534660b017db78a6f37f2
Alex Gallardo <100795457+alexgallardo11@users.noreply.github.com>
2023-04-27 15:40:15 +0200

    MS-98: task chat messages (#26)
    * MS-98/ write messages in task chat
    
    * MS-98/ screen bug fix
    
    * MS-98/ show only task messages,
    
    * MS-98/ order
    
    * MS-98/ create composable fun for TopBar and TextField, create use case for show only TaskChatMessages in TaskChatContent, refactor GetMessages
    
    * MS-98/ test whit turbine GetTaskChatMessagesImplTest, fix test GetMessagesImplTest
    
    * MS-98/ fix changes requested

--- androidApp/src/main/java/co/softov/morestuff/android/di/DomainModule.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/di/DomainModule.kt
@@ -127,6 +127,7 @@ val messageUseCases = module {
     factoryOf(::CreateScheduleMessageUseCaseImpl) bind CreateScheduleMessageUseCase::class
     factoryOf(::ClearActivePendingMessagesImpl) bind ClearActiveReminderMessages::class
     factoryOf(::CountActiveReminderMessagesImpl) bind CountActiveReminderMessages::class
+    factoryOf(::GetTaskChatMessagesUseCaseImpl) bind GetTaskChatMessagesUseCase::class
 }
 
 val priorityUseCases = module {
--- androidApp/src/main/java/co/softov/morestuff/android/ui/chat/task/TaskChatViewModel.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/chat/task/TaskChatViewModel.kt
@@ -6,27 +6,36 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
 import arrow.core.getOrElse
 import co.softov.morestuff.android.app.presentation.viewmodel.NoStateViewModel
+import co.softov.morestuff.android.domain.enums.ContentType
 import co.softov.morestuff.android.domain.enums.ReplyType
-import co.softov.morestuff.android.domain.model.*
+import co.softov.morestuff.android.domain.model.Message
+import co.softov.morestuff.android.domain.model.Schedule
+import co.softov.morestuff.android.domain.model.Task
 import co.softov.morestuff.android.domain.redux.middleware.ReminderAction.UserResponseAction
 import co.softov.morestuff.android.domain.redux.middleware.TaskAction
-import co.softov.morestuff.android.domain.usecase.message.GetMessages
+import co.softov.morestuff.android.domain.usecase.message.CreateMessageUseCase
+import co.softov.morestuff.android.domain.usecase.message.GetTaskChatMessagesUseCase
 import co.softov.morestuff.android.domain.usecase.schedule.GetActiveScheduleFlowUseCase
 import co.softov.morestuff.android.domain.usecase.task.GetTaskFlowUseCase
 import co.softov.morestuff.android.domain.usecase.task.UpdateTaskTitleUseCase
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class TaskChatViewModel(
-    getMessages: GetMessages,
+    getTaskChatMessagesUseCase: GetTaskChatMessagesUseCase,
     getActiveScheduleFlow: GetActiveScheduleFlowUseCase,
     private val getTaskFlow: GetTaskFlowUseCase,
     private val updateTaskTitleUseCase: UpdateTaskTitleUseCase,
     private val taskId: Long,
+    private val createMessageUseCase: CreateMessageUseCase,
 ) : NoStateViewModel() {
 
     val messages: StateFlow<List<Message>> =
-        getMessages(taskId = taskId)
+        getTaskChatMessagesUseCase(taskId = taskId)
             .stateIn(
                 scope = viewModelScope,
                 started = SharingStarted.Eagerly,
@@ -75,6 +84,18 @@ class TaskChatViewModel(
     fun setTaskComplete(complete: Boolean) {
         store.dispatch(TaskAction.CompleteTaskAction(taskId = taskId, complete))
     }
+
+    fun sendMessageForTask(content: String) {
+        viewModelScope.launch {
+            createMessageUseCase(
+                taskId = taskId,
+                scheduleId = 0,
+                title = content,
+                contentType = ContentType.TASK_MESSAGE,
+            )
+        }
+    }
+
 
     fun onBackPressed() {
         router.exit()
