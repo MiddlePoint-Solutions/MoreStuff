a36a20ff316cba004959e0ef8efed1e9a2d2a8d1
alexgallardo11 <alexgallardo11@gmail.com>
2023-04-28 12:10:23 +0200

    MS-103/ Task chat input should resize to wrap text

--- androidApp/src/main/java/co/softov/morestuff/android/ui/chat/task/TaskChatContent.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/chat/task/TaskChatContent.kt
@@ -402,7 +402,7 @@ fun TaskMessageTextField(
                     messageText = ""
                 }
             }),
-            maxLines = 1,
+            maxLines = Int.MAX_VALUE,
             cursorBrush = SolidColor(LocalContentColor.current),
             textStyle = LocalTextStyle.current.copy(
                 color = LocalContentColor.current,
