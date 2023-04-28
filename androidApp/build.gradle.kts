8fc93fa2101010be7d0fa19acd6e3a6595d1f1a6
Joe3112 <jhsagiv@gmail.com>
2023-04-28 18:00:45 +0200

    Prepare next release version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         compileSdk = 33
         minSdk = 25
         targetSdk = 33
-        versionCode = 7
-        versionName = "0.3.4"
+        versionCode = 8
+        versionName = "0.3.5"
     }
 
     buildTypes {
