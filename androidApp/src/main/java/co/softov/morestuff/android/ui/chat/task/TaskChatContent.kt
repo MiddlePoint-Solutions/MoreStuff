a737a1096c7bc00749f8dcbc6ab055158975f279
alexgallardo11 <alexgallardo11@gmail.com>
2023-05-25 17:01:31 +0200

    scheduling task: SchedulePermissionRequester

--- androidApp/src/main/java/co/softov/morestuff/android/ui/chat/task/TaskChatContent.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/chat/task/TaskChatContent.kt
@@ -86,6 +86,7 @@ import co.softov.morestuff.android.ui.main.PlanModel
 import co.softov.morestuff.android.ui.priority.PriorityButton
 import co.softov.morestuff.android.ui.priority.PriorityDatePicker
 import co.softov.morestuff.android.ui.priority.PriorityTimePicker
+import co.softov.morestuff.android.ui.priority.SchedulePermissionRequester
 import co.softov.morestuff.android.ui.priority.getRelativeDate
 import co.softov.morestuff.android.ui.theme.MoreStuffTheme
 import com.google.accompanist.insets.ui.Scaffold
@@ -430,6 +431,7 @@ fun ScheduleButton(
                 )
             }
         }
+        SchedulePermissionRequester()
     }
 }
 
