f6c86b33b55a8dc04d9811a7332ba34e55de1752
Joe3112 <jhsagiv@gmail.com>
2023-06-21 17:36:59 +0200

    Prepare next release 0.4.2-mvp

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         compileSdk = 33
         minSdk = 25
         targetSdk = 33
-        versionCode = 12
-        versionName = "0.4.1-mvp"
+        versionCode = 13
+        versionName = "0.4.2-mvp"
     }
 
     buildTypes {
