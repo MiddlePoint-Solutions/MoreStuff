79a603581e0d9c759490ddd8a204af08f1e3ea2f
Alex Gallardo <100795457+alexgallardo11@users.noreply.github.com>
2023-07-10 12:56:37 +0200

    Fix entering text with "." displays as url (#76)

--- androidApp/src/main/java/co/softov/morestuff/android/ui/utils/UiUtils.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/utils/UiUtils.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 import java.util.regex.Pattern
 
-const val URL_PATTERN_REGEX = "(https?://|www\\.|[a-zA-Z0-9_-]+\\.)?[\\w-]+(\\.[\\w-]+)+([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?"
+const val URL_PATTERN_REGEX = "(https?://|www\\.|[a-zA-Z0-9_-]+\\.)?[a-zA-Z0-9_-]+(\\.[a-zA-Z]+)+([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?"
 val urlPattern: Pattern = Pattern.compile(URL_PATTERN_REGEX, Pattern.CASE_INSENSITIVE or Pattern.MULTILINE or Pattern.DOTALL)
 
 
