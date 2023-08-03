41e374d2f518839ab27cea9bb3c0a3c528f0592f
alexgallardo11 <alexgallardo11@gmail.com>
2023-08-03 13:26:36 +0200

    show date

--- androidApp/src/main/java/co/softov/morestuff/android/data/utils/TimeFormatterImpl.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/data/utils/TimeFormatterImpl.kt
@@ -3,6 +3,7 @@ package co.softov.morestuff.android.data.utils
 import android.content.Context
 import android.text.format.DateFormat.is24HourFormat
 import co.softov.morestuff.android.domain.util.TimeFormatter
+import kotlinx.datetime.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -37,10 +38,38 @@ class TimeFormatterImpl(
         return formatTime(timeString, "EEEE d MMMM, HH:mm")
     }
 
+
     override fun formatToDateTime(timeString: String?): String? {
         return formatTime(timeString, "dd/MM/yyyy HH:mm${addAmPm()}")
     }
 
     private fun addAmPm() = if (is24HourFormat) "a" else ""
+
+    override fun formatTimeWithDayMonthYear(timeString: String?): String? {
+        return timeString?.let {
+            val localDateTime = LocalDateTime.parse(it)
+            formatWithOrdinal(localDateTime)
+        }
+    }
+
+    private fun formatWithOrdinal(dateTime: LocalDateTime): String {
+        val dayOfMonth = dateTime.dayOfMonth
+        val month = dateTime.month.name.lowercase().replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+        val year = dateTime.year
+
+        return "$dayOfMonth${getDayOfMonthSuffix(dayOfMonth)} $month, $year"
+    }
+
+    private fun getDayOfMonthSuffix(n: Int): String {
+        if (n in 11..13) {
+            return "th"
+        }
+        return when (n % 10) {
+            1 -> "st"
+            2 -> "nd"
+            3 -> "rd"
+            else -> "th"
+        }
+    }
 
 }
--- androidApp/src/main/java/co/softov/morestuff/android/ui/chat/ChatMessages.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/chat/ChatMessages.kt
@@ -2,14 +2,21 @@ package co.softov.morestuff.android.ui.chat
 
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDownward
@@ -26,11 +33,19 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import co.softov.morestuff.android.data.service.TimeManagerImpl
+import co.softov.morestuff.android.data.utils.TimeFormatterImpl
 import co.softov.morestuff.android.domain.enums.ContentType
 import co.softov.morestuff.android.domain.model.Message
+import co.softov.morestuff.android.domain.service.TimeManager
+import co.softov.morestuff.android.domain.util.TimeFormatter
 import co.softov.morestuff.android.ui.chat.items.AppChatItem
 import co.softov.morestuff.android.ui.chat.items.TaskReminderItem
 import co.softov.morestuff.android.ui.chat.items.UserChatItem
@@ -41,7 +56,7 @@ private val jumpToBottomThreshold = 56.dp
 private fun isAutoScrollingEnabled(
     pagingItemsCount: Int,
     itemsCount: Int,
-    scrollState: LazyListState
+    scrollState: LazyListState,
 ) = (pagingItemsCount > itemsCount) && scrollState.firstVisibleItemIndex == 0
 
 @Composable
@@ -49,12 +64,15 @@ fun Messages(
     messages: List<Message>,
     actions: ChatActions,
     modifier: Modifier = Modifier,
-    scrollState: LazyListState
+    scrollState: LazyListState,
 ) {
     val scope = rememberCoroutineScope()
     var itemsCount by remember { mutableIntStateOf(0) }
     val enableAutoScroll = isAutoScrollingEnabled(messages.size, itemsCount, scrollState)
     itemsCount = messages.size
+    val timeManager: TimeManager = TimeManagerImpl()
+    val timeFormatter: TimeFormatter = TimeFormatterImpl(context = LocalContext.current)
+
 
     Box(modifier = modifier) {
         LazyColumn(
@@ -63,16 +81,55 @@ fun Messages(
             state = scrollState,
             contentPadding = PaddingValues(top = 10.dp, bottom = 20.dp)
         ) {
-            items(
+            itemsIndexed(
                 items = messages,
-                key = { item -> item.id }
-            ) { item ->
+                key = { _, item -> item.id }
+            ) { index, item ->
+                val nextMessage = if (index < messages.size - 1) messages[index + 1] else null
 
-                when (item.contentType) {
-                    ContentType.USER_NEW_TASK -> UserChatItem(message = item, actions)
-                    ContentType.CONFIRM_NEW_TASK -> AppChatItem(message = item, actions)
-                    ContentType.TASK_REMINDER -> TaskReminderItem(message = item, actions = actions)
-                    ContentType.TASK_MESSAGE -> UserChatItem(message = item, actions = actions)
+                val currentMessageDateTime = timeManager.utcStringToLocalDateTime(item.createTime)
+                val nextMessageDateTime =
+                    nextMessage?.let { timeManager.utcStringToLocalDateTime(it.createTime) }
+
+                val isLastMessageOfDay = currentMessageDateTime.date != nextMessageDateTime?.date
+
+                Column {
+                    if (isLastMessageOfDay) {
+                        Row(
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp, vertical = 4.dp)
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            timeFormatter.formatTimeWithDayMonthYear(
+                                currentMessageDateTime.toString(),
+                            )
+                                ?.let {
+                                    Text(
+                                        text = it,
+                                        modifier = Modifier
+                                            .background(
+                                                color = Color.Gray.copy(alpha = 0.5f),
+                                                shape = RoundedCornerShape(50)
+                                            )
+                                            .padding(horizontal = 8.dp, vertical = 4.dp),
+                                        fontSize = 17.sp,
+                                        color = Color.White,
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                        }
+                    }
+                    when (item.contentType) {
+                        ContentType.USER_NEW_TASK -> UserChatItem(message = item, actions)
+                        ContentType.CONFIRM_NEW_TASK -> AppChatItem(message = item, actions)
+                        ContentType.TASK_REMINDER -> TaskReminderItem(
+                            message = item,
+                            actions = actions
+                        )
+
+                        ContentType.TASK_MESSAGE -> UserChatItem(message = item, actions = actions)
+                    }
                 }
             }
         }
@@ -112,6 +169,7 @@ fun Messages(
     }
 }
 
+
 private enum class Visibility {
     VISIBLE,
     GONE
@@ -124,7 +182,7 @@ private enum class Visibility {
 fun JumpToBottom(
     enabled: Boolean,
     onClicked: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     // Show Jump to Bottom button
     val transition = updateTransition(
