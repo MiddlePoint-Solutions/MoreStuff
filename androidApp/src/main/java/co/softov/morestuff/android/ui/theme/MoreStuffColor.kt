b9f0b18db34832afa95399b938f7e21d4c04d65c
Joe3112 <jhsagiv@gmail.com>
2023-08-10 16:21:20 +0200

    Fix home screen background & user input color

--- androidApp/src/main/java/co/softov/morestuff/android/ui/theme/MoreStuffColor.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/theme/MoreStuffColor.kt
@@ -2,28 +2,8 @@ package co.softov.morestuff.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Gray100 = Color(0xfff5f5f5)
-val BlueGray600 = Color(0xff546e7a)
-val BlueGray900 = Color(0xff29434e)
-
-val Indigo200 = Color(0xff8e99f3)
-val Indigo400 = Color(0xff5c6bc0)
-
-val Orange300 = Color(0xffffb74d)
-val Orange500 = Color(0xffff9800)
-val Orange600 = Color(0xfffb8c00)
-
-val Red200 = Color(0xfff297a2)
-val Red800 = Color(0xffd00036)
-
 val BlueDark = Color(0xFFB3E5FC)
 val BlueLight = Color(0xFF303F9F)
-
-val primaryLightTheme = Color(0xFFEFF2FA)
-val primaryContainerLightTheme =Color(0xFFB8C1DF)
-val secondaryContainerLightTheme = Color(0xFF6A7493)
-
-val priorityAction = Color(0xFF5C5E74)
 
 val reviewIconTint = Color(140, 152, 255)
 
