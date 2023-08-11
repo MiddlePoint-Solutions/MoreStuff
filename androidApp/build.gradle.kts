7b448e69564cbe9b887b350af5692e680aa626ee
Joe3112 <jhsagiv@gmail.com>
2023-08-11 18:51:46 +0200

    Prepare next release (0.4.7)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         compileSdk = 33
         minSdk = 26
         targetSdk = 33
-        versionCode = 17
-        versionName = "0.4.6"
+        versionCode = 18
+        versionName = "0.4.7"
         vectorDrawables {
             useSupportLibrary = true
         }
