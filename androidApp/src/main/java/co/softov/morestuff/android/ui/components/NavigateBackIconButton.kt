7fef80261fcab42298ec2a256aa2bf4e6d52141d
alexgallardo11 <alexgallardo11@gmail.com>
2023-08-14 13:24:25 +0200

    Fix color of the topbar icons in the Import & preview screens in Light theme

--- androidApp/src/main/java/co/softov/morestuff/android/ui/components/NavigateBackIconButton.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/components/NavigateBackIconButton.kt
@@ -4,8 +4,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import co.softov.morestuff.android.R
 
@@ -15,7 +15,7 @@ fun NavigateBackIconButton(onBack: () -> Unit) {
         Icon(
             imageVector = Icons.Filled.ArrowBack,
             contentDescription = stringResource(id = R.string.cd_navigate_back),
-            tint = Color.White
+            tint = MaterialTheme.colorScheme.onSurface
         )
     }
 }
