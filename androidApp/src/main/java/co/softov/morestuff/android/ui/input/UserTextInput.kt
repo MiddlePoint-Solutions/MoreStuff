d81299e6466cbec076f2f5d4ea06b1361f82e99b
Alex Gallardo <100795457+alexgallardo11@users.noreply.github.com>
2023-08-16 20:46:28 +0200

    Add VoiceToText for task chat input (#106)

--- androidApp/src/main/java/co/softov/morestuff/android/ui/input/UserTextInput.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/input/UserTextInput.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,6 +39,9 @@ import co.softov.morestuff.android.app.presentation.extension.clearFocusOnKeyboa
 import co.softov.morestuff.android.ui.components.SendIcon
 import co.softov.morestuff.android.ui.theme.MoreStuffTheme
 
+
+val LocalBoxWeight = compositionLocalOf { 0.12f }
+
 @Composable
 fun UserTextInput(
     value: TextFieldValue,
@@ -49,6 +53,10 @@ fun UserTextInput(
 ) {
 
     val a11ylabel = stringResource(id = R.string.textfield_desc)
+
+    val boxWeight = LocalBoxWeight.current
+
+
 
     Surface(
         modifier = modifier
@@ -111,7 +119,7 @@ fun UserTextInput(
 
                 Box(
                     modifier = Modifier
-                        .weight(0.12f)
+                        .weight(boxWeight)
                         .height(IntrinsicSize.Min)
                         .align(Alignment.Bottom)
                         .padding(4.dp).padding(end = 7.dp)
