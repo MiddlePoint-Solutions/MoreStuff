32e987f2090af3768f18c548974e99631c730189
Joe3112 <jhsagiv@gmail.com>
2023-08-21 10:20:00 +0200

    Fix priority list bottom padding issue

--- androidApp/src/main/java/co/softov/morestuff/android/ui/schedule/PriorityItem.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/schedule/PriorityItem.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -49,7 +50,7 @@ fun PriorityItem(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(max = 90.dp)
+            .height(80.dp)
             .shadow(elevation = elevation)
             .background(color = MaterialTheme.colorScheme.surfaceContainer)
     ) {
@@ -96,9 +97,9 @@ fun TaskProfile(task: TaskDomain) {
 
     Box(
         modifier = Modifier
-            .padding(start = 14.dp, top = 14.dp, bottom = 14.dp)
+            .padding(start = 14.dp)
             .clip(CircleShape)
-            .size(58.dp)
+            .size(50.dp)
             .background(
                 brush = Brush.verticalGradient(profileColor)
             )
