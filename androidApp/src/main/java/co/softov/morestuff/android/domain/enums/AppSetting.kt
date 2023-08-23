22dedb3250b0a0d06c184eade9d49b39924ead1e
Joe3112 <jhsagiv@gmail.com>
2023-08-23 06:15:30 +0200

    Fix AppTheme setting

--- androidApp/src/main/java/co/softov/morestuff/android/domain/enums/AppSetting.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/domain/enums/AppSetting.kt
@@ -2,7 +2,7 @@ package co.softov.morestuff.android.domain.enums
 
 sealed class AppSetting<T>(val defaultValue: T) {
     data object FirstTime : AppSetting<Boolean>(true)
-    data object Theme : AppSetting<AppTheme>(AppTheme.System)
+    data object Theme : AppSetting<String>(AppTheme.System.name)
     data object SnoozeLimit : AppSetting<Int>(3)
     data object Confetti : AppSetting<Boolean>(true)
 
--- androidApp/src/main/java/co/softov/morestuff/android/domain/usecase/settings/GetAppSettingUseCase.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/domain/usecase/settings/GetAppSettingUseCase.kt
@@ -4,13 +4,13 @@ import co.softov.morestuff.android.domain.enums.AppSetting
 import co.softov.morestuff.android.domain.repository.UserRepository
 
 interface GetAppSettingUseCase {
-    operator fun<T> invoke(setting: AppSetting<T>): T
+    operator fun<T, R> invoke(setting: AppSetting<T>): R
 }
 
 class GetAppSettingUseCaseImpl(
     private val userRepository: UserRepository
 ) : GetAppSettingUseCase {
-    override fun<T> invoke(setting: AppSetting<T>): T {
+    override fun<T, R> invoke(setting: AppSetting<T>): R {
         return userRepository.getAppSetting(setting)
     }
 
