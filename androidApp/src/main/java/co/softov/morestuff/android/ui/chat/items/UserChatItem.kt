cd9081bd35ddf48494c4ed433854161d4f8747e3
Alex Gallardo <100795457+alexgallardo11@users.noreply.github.com>
2023-09-19 11:44:11 +0200

    change url color (#132)
    * change url color
    
    * changes requested

--- androidApp/src/main/java/co/softov/morestuff/android/ui/chat/items/UserChatItem.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/chat/items/UserChatItem.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -61,13 +62,13 @@ fun UserChatItem(
     var showMenu by remember { mutableStateOf(false) }
     val uriHandler = LocalUriHandler.current
     val coroutineScope = rememberCoroutineScope()
-    val urlPattern = urlPattern
     val openGraphResult = message.openGraphResult
+    val urlColor = rememberUpdatedState(MaterialTheme.colorScheme.onPrimary)
 
     val content by remember {
         derivedStateOf {
             buildAnnotatedString {
-                appendUrlsWithStyle(message.content, urlPattern)
+                appendUrlsWithStyle(message.content, urlPattern, urlColor.value)
             }
         }
     }
