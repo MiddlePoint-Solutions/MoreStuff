679a25306d863b77a58b77e41bb5217f2ed82c57
alexgallardo11 <alexgallardo11@gmail.com>
2023-10-18 09:56:37 +0200

    changes requested

--- androidApp/src/main/java/co/softov/morestuff/android/ui/onboarding/OnBoardingReviewScreenComplements.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/onboarding/OnBoardingReviewScreenComplements.kt
@@ -1,6 +1,5 @@
 package co.softov.morestuff.android.ui.onboarding
 
-import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,23 +9,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
-import co.softov.morestuff.android.presentation.presenter.ReviewRound
-import co.softov.morestuff.android.ui.compose.SlideAnimation
 import co.softov.morestuff.android.ui.model.ReviewItemUiModel
-import co.softov.morestuff.android.ui.review.ReviewViewModel
 import co.softov.morestuff.android.ui.review.TaskCard
-import co.softov.morestuff.android.ui.review.firstVisibleStateOrNull
 import co.softov.morestuff.android.ui.review.swipeable.ExperimentalSwipeableCardApi
 import co.softov.morestuff.android.ui.review.swipeable.SwipeDirection
 import co.softov.morestuff.android.ui.review.swipeable.SwipeableCardState
@@ -34,74 +27,38 @@ import co.softov.morestuff.android.ui.review.swipeable.rememberSwipeableCardStat
 import co.softov.morestuff.android.ui.review.swipeable.swipableCard
 import co.softov.morestuff.android.ui.theme.surfaceContainer
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
- fun ReviewCardsOnBoarding(tasks: List<ReviewItemUiModel>) {
-    val viewModel: ReviewViewModel = koinViewModel()
-    val scope = rememberCoroutineScope()
+fun ReviewCardsOnBoarding(tasks: List<ReviewItemUiModel>) {
 
-    LaunchedEffect(Unit) {
-        viewModel.loadData()
-    }
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+    ) {
 
-    Box{
-        val model by viewModel.uiModel.collectAsState()
+        val states = tasks.map { it to rememberSwipeableCardState() }
 
         Box(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.surfaceContainer)
+                .size(400.dp),
+            contentAlignment = Alignment.Center
         ) {
-            when (model.round) {
-                ReviewRound.Review -> {
-                    val states = tasks.map { it to rememberSwipeableCardState(model.round) }
-                    val visibleState = remember(model.round) { MutableTransitionState(false) }
-
-                     SlideAnimation(
-                         visibleState = visibleState,
-                         modifier = Modifier
-                             .fillMaxHeight(0.30f)
-                             .align(Alignment.BottomCenter)
-                     ) {
-                     }
-                    Box(
-                        modifier = Modifier
-                            .size(400.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        OnBoardingTaskPrioritySwipe(
-                            modifier = Modifier
-                                .offset(y = 32.dp)
-                                .fillMaxHeight(1f),
-                            states = states,
-                            onComplete = {
-                                scope.launch {
-                                    states.firstVisibleStateOrNull()?.onComplete()
-                                    viewModel.completeTask(it)
-                                }
-                            }
-                        )
-                    }
-
-                    LaunchedEffect(key1 = model.round) {
-                        visibleState.targetState = true
-                    }
-                }
-
-                else -> {}
-            }
+            OnBoardingTaskPrioritySwipe(
+                modifier = Modifier
+                    .offset(y = 32.dp)
+                    .fillMaxHeight(1f),
+                states = states,
+            )
         }
     }
 }
 
 @Composable
 @OptIn(ExperimentalSwipeableCardApi::class)
-fun OnBoardingTaskPrioritySwipe(
+private fun OnBoardingTaskPrioritySwipe(
     modifier: Modifier = Modifier,
     states: List<Pair<ReviewItemUiModel, SwipeableCardState>>,
-    onComplete: (ReviewItemUiModel) -> Unit,
 ) {
     Box(modifier = modifier.padding(20.dp)) {
         var cardAnimated by remember { mutableStateOf(states.size - 1) }
@@ -114,8 +71,9 @@ fun OnBoardingTaskPrioritySwipe(
                     .layoutId(task.id)
                     .swipableCard(state = state),
                 task = task,
-                onComplete = onComplete,
-                isVisible = true
+                onComplete = {},
+                isVisible = true,
+                isClickable = false
             )
 
             if (index == cardAnimated && state.swipedDirection == null) {
@@ -132,7 +90,6 @@ fun OnBoardingTaskPrioritySwipe(
         }
     }
 }
-
 
 
 private fun SwipeDirection(position: Int): SwipeDirection {
