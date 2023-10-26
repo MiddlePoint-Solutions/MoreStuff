4c0d85fefee32c45d548d26370a6f26f1944f85b
alexgallardo11 <alexgallardo11@gmail.com>
2023-10-26 14:38:37 +0200

    Remove drag & drop functionality

--- androidApp/src/main/java/co/softov/morestuff/android/ui/schedule/PriorityItem.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/schedule/PriorityItem.kt
@@ -1,7 +1,6 @@
 package co.softov.morestuff.android.ui.schedule
 
 import android.content.res.Configuration
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -18,7 +17,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -27,26 +25,22 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import co.softov.morestuff.android.domain.model.ScheduleDomain
 import co.softov.morestuff.android.domain.enums.ScheduleType
+import co.softov.morestuff.android.domain.model.ScheduleDomain
 import co.softov.morestuff.android.domain.model.TaskDomain
 import co.softov.morestuff.android.ui.theme.MoreStuffTheme
 import co.softov.morestuff.android.ui.theme.surfaceContainer
-import timber.log.Timber
 
 @Composable
 fun PriorityItem(
     task: TaskDomain,
     onClick: (taskId: Long) -> Unit,
     modifier: Modifier = Modifier,
-    isDragging: Boolean = false,
 ) {
-    val elevation = animateDpAsState(if (isDragging) 16.dp else 0.dp, label = "").value
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(80.dp)
-            .shadow(elevation = elevation)
             .background(color = MaterialTheme.colorScheme.surfaceContainer)
             .clickable { onClick(task.id) }
     ) {
