fc9c527a741aa890cbab24e40a0421744e6b01b5
Alex Gallardo <100795457+alexgallardo11@users.noreply.github.com>
2023-11-09 09:57:59 +0100

    feature/ms-79-review-not-ending-when-last-item-is-marked-done (#172)
    * solved conflicts
    
    * refactor

--- androidApp/src/main/java/co/softov/morestuff/android/ui/review/ReviewViewModel.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/review/ReviewViewModel.kt
@@ -126,6 +126,16 @@ class ReviewViewModel(
     fun completeTask(item: ReviewItemUiModel) {
         sendEvent(ItemReview(item, PriorityActionType.Done))
         dispatchAppStoreAction(TaskAction.CompleteTasksAction(listOf(item.id), true))
+        goToFinalRound(item)
+    }
+
+    private fun goToFinalRound(item: ReviewItemUiModel) {
+        if (state.items.first() == item) {
+            roundEndDelayJob = viewModelScope.launch {
+                delay(500)
+                sendEvent(SetupRound(Final))
+            }
+        }
     }
 
     fun toggleHintArrowPriority() {
