1e49d910d69f1442531c8820749aa07a4a81b34b
alexgallardo11 <alexgallardo11@gmail.com>
2023-11-14 10:41:29 +0100

    Refactor user chat item

--- androidApp/src/main/java/co/softov/morestuff/android/ui/chat/items/UserChatItem.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/chat/items/UserChatItem.kt
@@ -15,8 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowRightAlt
-import androidx.compose.material.icons.filled.Chat
 import androidx.compose.material.icons.filled.Start
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
@@ -25,13 +23,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,6 +45,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.softov.morestuff.android.domain.enums.ContentType
 import co.softov.morestuff.android.ui.chat.ChatActions
+import co.softov.morestuff.android.ui.chat.items.MockData.messageUiModel
 import co.softov.morestuff.android.ui.chat.task.MessageUiModel
 import co.softov.morestuff.android.ui.theme.MoreStuffTheme
 import co.softov.morestuff.android.ui.utils.appendUrlsWithStyle
@@ -57,35 +56,15 @@ import coil.request.ImageRequest
 import coil.size.Scale
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
+
 @Composable
 fun UserChatItem(
     messageUiModel: MessageUiModel,
     actions: ChatActions,
 ) {
-    val message = messageUiModel.message
-    var showMenu by remember { mutableStateOf(false) }
-    val uriHandler = LocalUriHandler.current
-    val coroutineScope = rememberCoroutineScope()
-    val openGraphResult = message.openGraphResult
-    val urlColor = rememberUpdatedState(MaterialTheme.colorScheme.onPrimary)
-
-    val content by remember {
-        derivedStateOf {
-            buildAnnotatedString {
-                appendUrlsWithStyle(message.content, urlPattern, urlColor.value)
-            }
-        }
-    }
-
-    val urls by remember {
-        derivedStateOf {
-            content.getStringAnnotations("URL", start = 0, end = content.length).firstOrNull()?.item
-        }
-    }
-
+    val showMenu = remember { mutableStateOf(false) }
     val isNewUserTask by remember {
-        derivedStateOf { message.contentType == ContentType.USER_NEW_TASK }
+        derivedStateOf { messageUiModel.message.contentType == ContentType.USER_NEW_TASK }
     }
 
     Column(
@@ -99,142 +78,27 @@ fun UserChatItem(
             Box(
                 modifier = Modifier
                     .padding(end = 5.dp, bottom = 4.dp)
-                    .clickable { showMenu = true }
+                    .clickable { showMenu.value = true }
             ) {
-                if (message.isDataMessage) {
-                    Surface(
-                        shape = RoundedCornerShape(
-                            topStart = 10.dp,
-                            topEnd = 10.dp,
-                            bottomEnd = 7.dp,
-                            bottomStart = 10.dp
-                        ),
-                        color = MaterialTheme.colorScheme.primary,
-                        modifier = Modifier.padding(2.dp)
-                    ) {
-                        Column(
-                            modifier = Modifier.align(Alignment.CenterEnd),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.End
-                        ) {
-
-                            val uri by remember {
-                                derivedStateOf { Uri.parse(message.messageData?.filePath) }
-                            }
-
-                            Image(
-                                painter = rememberAsyncImagePainter(
-                                    model = ImageRequest.Builder(LocalContext.current)
-                                        .data(data = uri)
-                                        .crossfade(true)
-                                        .scale(Scale.FIT)
-                                        .memoryCacheKey(message.messageData?.filePath)
-                                        .build(),
-                                    imageLoader = LocalContext.current.imageLoader
-                                ),
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .padding(start = 1.dp, top = 1.dp, end = 1.dp, bottom = 10.dp)
-                                    .combinedClickable(
-                                        onClick = { actions.onImageSelected(message) },
-                                        onLongClick = { showMenu = true }
-                                    )
-                                    .sizeIn(minHeight = 200.dp, maxHeight = 400.dp)
-                                    .clip(RoundedCornerShape(8.dp)),
-                                contentScale = ContentScale.Inside
-                            )
-
-                            Column {
-                                if (message.content.isNotEmpty()) {
-                                    Text(
-                                        text = message.content,
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(
-                                                start = 14.dp,
-                                                end = 15.dp,
-                                                bottom = 3.dp
-                                            ),
-                                        style = LocalTextStyle.current.copy(
-                                            color = MaterialTheme.colorScheme.onPrimary,
-                                            fontSize = 16.sp
-                                        )
-                                    )
-                                }
-                                Row(Modifier.align(Alignment.End)) {
-                                    MessageTime(
-                                        formattedTimeOnly = messageUiModel.formattedTimeOnly,
-                                        modifier = Modifier
-                                    )
-                                }
-                            }
-                        }
-                    }
-
-                } else {
-                    Surface(
-                        shape = RoundedCornerShape(
-                            topStart = 14.dp,
-                            topEnd = 14.dp,
-                            bottomEnd = 5.dp,
-                            bottomStart = 14.dp
-                        ),
-                        color = MaterialTheme.colorScheme.primary,
-                    ) {
-                        Column {
-                            Text(
-                                modifier = Modifier
-                                    .padding(
-                                        start = 14.dp,
-                                        end = 15.dp,
-                                        top = 8.dp,
-                                        bottom = 3.dp
-                                    )
-                                    .run {
-                                        urls?.let {
-                                            combinedClickable(
-                                                onClick = {
-                                                    coroutineScope.launch {
-                                                        uriHandler.openUri(it)
-                                                    }
-                                                },
-                                                onLongClick = { showMenu = true }
-                                            )
-                                        } ?: this
-                                    },
-                                text = content,
-                                style = LocalTextStyle.current.copy(
-                                    color = MaterialTheme.colorScheme.onPrimary,
-                                    fontSize = 16.sp
-                                )
-                            )
-                            if (openGraphResult?.title != null && openGraphResult.description != null) {
-                                OpenGraphView(openGraphResult)
-                            }
-                            Row(Modifier.align(Alignment.End)) {
-                                MessageTime(
-                                    formattedTimeOnly = messageUiModel.formattedTimeOnly,
-                                    modifier = Modifier
-                                )
-                            }
-                        }
-                    }
-                }
+                MessageContent(
+                    messageUiModel = messageUiModel,
+                    actions = actions,
+                    showMenu = showMenu,
+                )
 
                 ShowContextMenu(
-                    message,
-                    showMenu = showMenu,
+                    messageUiModel.message,
+                    showMenu = showMenu.value,
                     copyMessage = actions.copyMessage,
                     deleteMessage = actions.deleteMessage,
-                    close = { showMenu = false },
+                    close = { showMenu.value = false },
                     shareImage = actions.shareImage,
                     shareMessage = actions.shareMessage
                 )
             }
         }
-
         if (isNewUserTask) {
-            FilledIconButton(onClick = { actions.taskChatAction(message.taskId) }) {
+            FilledIconButton(onClick = { actions.taskChatAction(messageUiModel.message.taskId) }) {
                 Icon(
                     imageVector = Icons.Default.Start,
                     contentDescription = ""
@@ -244,6 +108,173 @@ fun UserChatItem(
 
     }
 }
+
+@Composable
+fun MessageContent(
+    messageUiModel: MessageUiModel,
+    actions: ChatActions,
+    showMenu: MutableState<Boolean>,
+) {
+    if (messageUiModel.message.isDataMessage) {
+        MessageImage(
+            message = messageUiModel,
+            actions = actions,
+            showMenu = showMenu,
+        )
+    } else {
+        val openGraphResult = messageUiModel.message.openGraphResult
+        Surface(
+            shape = RoundedCornerShape(
+                topStart = 14.dp,
+                topEnd = 14.dp,
+                bottomEnd = 5.dp,
+                bottomStart = 14.dp
+            ),
+            color = MaterialTheme.colorScheme.primary
+        ) {
+            Column {
+                MessageText(
+                    showMenu = showMenu,
+                    messageUiModel = messageUiModel,
+                    modifier = Modifier
+                        .padding(
+                            start = 14.dp,
+                            end = 15.dp,
+                            top = 8.dp,
+                            bottom = 3.dp
+                        )
+                )
+                if (openGraphResult?.title != null && openGraphResult.description != null) {
+                    OpenGraphView(openGraphResult)
+                }
+                Row(Modifier.align(Alignment.End)) {
+                    MessageTime(
+                        formattedTimeOnly = messageUiModel.formattedTimeOnly,
+                        modifier = Modifier
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MessageImage(
+    message: MessageUiModel,
+    actions: ChatActions,
+    showMenu: MutableState<Boolean>,
+) {
+    Surface(
+        shape = RoundedCornerShape(
+            topStart = 10.dp,
+            topEnd = 10.dp,
+            bottomEnd = 7.dp,
+            bottomStart = 10.dp
+        ),
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(2.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.End
+        ) {
+            val uri by remember {
+                derivedStateOf { Uri.parse(message.message.messageData?.filePath) }
+            }
+
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(data = uri)
+                        .crossfade(true)
+                        .scale(Scale.FIT)
+                        .memoryCacheKey(message.message.messageData?.filePath)
+                        .build(),
+                    imageLoader = LocalContext.current.imageLoader
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(start = 1.dp, top = 1.dp, end = 1.dp, bottom = 10.dp)
+                    .combinedClickable(
+                        onClick = { actions.onImageSelected(message.message) },
+                        onLongClick = { showMenu.value = true }
+                    )
+                    .sizeIn(minHeight = 200.dp, maxHeight = 400.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Inside
+            )
+            if (message.message.content.isNotEmpty()) {
+                MessageText(
+                    showMenu = showMenu,
+                    messageUiModel = message,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Start)
+                        .padding(
+                            start = 14.dp,
+                            end = 15.dp,
+                        )
+                )
+            }
+            Row(Modifier.align(Alignment.End)) {
+                MessageTime(
+                    formattedTimeOnly = messageUiModel.formattedTimeOnly,
+                    modifier = Modifier
+                )
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MessageText(
+    messageUiModel: MessageUiModel,
+    showMenu: MutableState<Boolean>,
+    modifier: Modifier = Modifier,
+) {
+    val message = messageUiModel.message
+    val urlColor = rememberUpdatedState(MaterialTheme.colorScheme.onPrimary)
+    val content by remember {
+        derivedStateOf {
+            buildAnnotatedString {
+                appendUrlsWithStyle(message.content, urlPattern, urlColor.value)
+            }
+        }
+    }
+
+    val urls by remember {
+        derivedStateOf {
+            content.getStringAnnotations("URL", start = 0, end = content.length)
+                .firstOrNull()?.item
+        }
+    }
+    val scope = rememberCoroutineScope()
+    val uriHandler = LocalUriHandler.current
+    Text(
+        modifier = modifier
+            .run {
+                urls?.let {
+                    combinedClickable(
+                        onClick = {
+                            scope.launch {
+                                uriHandler.openUri(it)
+                            }
+                        },
+                        onLongClick = { showMenu.value = true }
+                    )
+                } ?: this
+            },
+        text = content,
+        style = LocalTextStyle.current.copy(
+            color = MaterialTheme.colorScheme.onPrimary,
+            fontSize = 16.sp
+        )
+    )
+}
+
 
 @Composable
 fun MessageTime(
@@ -277,6 +308,6 @@ fun MessageTime(
 @Composable
 fun UserChatItemPreview() {
     MoreStuffTheme {
-        UserChatItem(MockData.messageUiModel, ChatActions())
+        UserChatItem(messageUiModel, ChatActions())
     }
 }
