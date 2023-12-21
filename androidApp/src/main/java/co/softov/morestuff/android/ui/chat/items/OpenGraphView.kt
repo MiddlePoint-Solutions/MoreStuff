9a3b6502dacf19268d718db6bfb77c5e337c1691
Alex Gallardo <100795457+alexgallardo11@users.noreply.github.com>
2023-12-21 15:36:30 +0100

    feature/ms-111-redesign-link-preview-message (#196)
    * Redesign Link preview message
    
    * use Ksoup and Ktor for parsing the metadata
    
    * Use theme color for open graph background
    
    ---------
    
    Co-authored-by: Joe3112 <jhsagiv@gmail.com>

--- androidApp/src/main/java/co/softov/morestuff/android/ui/chat/items/OpenGraphView.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/chat/items/OpenGraphView.kt
@@ -1,63 +1,75 @@
 package co.softov.morestuff.android.ui.chat.items
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.softov.morestuff.android.domain.model.OpenGraphResult
+import co.softov.morestuff.android.ui.theme.isDarkTheme
 import coil.compose.rememberAsyncImagePainter
+import timber.log.Timber
+
 
 @Composable
 fun OpenGraphView(openGraphResult: OpenGraphResult) {
-    Column(
+    Box(
         modifier = Modifier
-            .padding(8.dp)
             .scale(0.9f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(
+                color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.1f)
+            ).padding(7.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
             Text(
                 text = openGraphResult.title ?: "",
                 style = TextStyle(
                     fontWeight = FontWeight.Bold,
-                    fontSize = 12.sp,
+                    fontSize = 9.sp,
                     color = MaterialTheme.colorScheme.onPrimary
-                ),
-                modifier = Modifier.weight(1f)
+                )
             )
+            Text(
+                text = openGraphResult.description ?: "",
+                fontSize = 9.sp,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+
             if (openGraphResult.image != null) {
-                Box(modifier = Modifier.size(40.dp)) {
+                Timber.d("Loading image: ${openGraphResult.image}")
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .padding(top = 8.dp)
+                ) {
                     Image(
                         painter = rememberAsyncImagePainter(openGraphResult.image),
                         contentDescription = null,
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(RoundedCornerShape(6.dp)),
+                        contentScale = ContentScale.Crop
                     )
                 }
             }
         }
-        Text(
-            text = openGraphResult.description ?: "",
-            fontSize = 9.sp,
-            modifier = Modifier.padding(top = 8.dp),
-            color = MaterialTheme.colorScheme.onPrimary
-        )
     }
 }
