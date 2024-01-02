d8ab37f31d57cb0b0953d347f4ef2c55bb50dae9
Joe3112 <jhsagiv@gmail.com>
2024-01-02 12:40:43 +0100

    UI tweaks

--- androidApp/src/main/java/co/softov/morestuff/android/ui/components/HomeTopBar.kt
+++ androidApp/src/main/java/co/softov/morestuff/android/ui/components/HomeTopBar.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -107,7 +108,10 @@ fun HomeTopBar(
                         }
                     }
                 } else {
-                    Text(text = stringResource(id = R.string.app_name))
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        fontWeight = FontWeight.SemiBold
+                    )
                 }
             }
         },
