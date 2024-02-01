1f70697558340318500d9330aeb69bd60e0959d5
Joe3112 <jhsagiv@gmail.com>
2024-02-01 14:13:32 +0100

    Prepare next release (v0.5.5)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         compileSdk = 34
         minSdk = 26
         targetSdk = 34
-        versionCode = 25
-        versionName = "0.5.4"
+        versionCode = 26
+        versionName = "0.5.5"
         vectorDrawables {
             useSupportLibrary = true
         }
