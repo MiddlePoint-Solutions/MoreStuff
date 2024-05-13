b52bb79fdc22f8195363ad1480254c827b84ea56
Joe3112 <jhsagiv@gmail.com>
2024-05-13 22:49:26 +0200

    Refactor ShareViewModel (#244)
    * WIP
    
    * Refactor ShareViewModel with Molecule

--- androidApp/src/main/kotlin/co/softov/morestuff/android/ui/share/ShareViewModel.kt
+++ androidApp/src/main/kotlin/co/softov/morestuff/android/ui/share/ShareViewModel.kt
@@ -1,9 +1,11 @@
 package co.softov.morestuff.android.ui.share
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
+import co.softov.morestuff.android.app.presentation.viewmodel.MoleculeViewModel
 import co.softov.morestuff.android.app.presentation.viewmodel.NoStateViewModel
 import co.softov.morestuff.android.domain.usecase.task.GetActiveTasksFlowUseCase
 import co.softov.morestuff.android.domain.usecase.task.SearchTasksUseCase
@@ -11,6 +13,7 @@ import co.softov.morestuff.android.ui.model.map.TaskUiMapper
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.debounce
@@ -21,45 +24,12 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class ShareViewModel(
-    getActiveTasksFlowUseCase: GetActiveTasksFlowUseCase,
-    private val searchTasksUseCase: SearchTasksUseCase,
-    private val taskUiMapper: TaskUiMapper,
-) : NoStateViewModel() {
+class ShareViewModel : MoleculeViewModel<ShareEvent, ShareState>() {
 
-    var query by mutableStateOf("")
-        private set
+    override val initialState: ShareState = ShareState()
 
-    val tasks = getActiveTasksFlowUseCase()
-        .map { taskUiMapper.map(it) }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Eagerly,
-            initialValue = listOf()
-        )
-
-    private val searchQueryFlow = MutableStateFlow("")
-
-    @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
-    val searchResults = searchQueryFlow
-        .debounce(300)
-        .distinctUntilChanged()
-        .flatMapLatest { searchTasksUseCase(it, true) }
-        .map { taskUiMapper.map(it) }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Lazily,
-            initialValue = listOf()
-        )
-
-    fun updateSearchQuery(searchQuery: String) {
-        searchQueryFlow.update { searchQuery }
-    }
-
-    fun resetSearchQuery() {
-        viewModelScope.launch {
-            delay(300)
-            searchQueryFlow.update { "" }
-        }
+    @Composable
+    override fun models(events: Flow<ShareEvent>): ShareState {
+        return shareModel(initialState, events)
     }
 }
