94ea44c3a81d1ad905b5a8da472a586f2fd8d774
Joe3112 <jhsagiv@gmail.com>
2024-05-13 18:09:32 +0200

    WIP

--- androidApp/src/main/kotlin/co/softov/morestuff/android/ui/share/ShareViewModel.kt
+++ androidApp/src/main/kotlin/co/softov/morestuff/android/ui/share/ShareViewModel.kt
@@ -1,16 +1,24 @@
 package co.softov.morestuff.android.ui.share
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
+import co.softov.morestuff.android.app.presentation.viewmodel.MoleculeViewModel
 import co.softov.morestuff.android.app.presentation.viewmodel.NoStateViewModel
+import co.softov.morestuff.android.domain.redux.AppStore
 import co.softov.morestuff.android.domain.usecase.task.GetActiveTasksFlowUseCase
 import co.softov.morestuff.android.domain.usecase.task.SearchTasksUseCase
+import co.softov.morestuff.android.ui.main.MainEvent
+import co.softov.morestuff.android.ui.main.MainState
+import co.softov.morestuff.android.ui.main.mainModel
+import co.softov.morestuff.android.ui.model.TaskUiModel
 import co.softov.morestuff.android.ui.model.map.TaskUiMapper
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.debounce
@@ -20,6 +28,37 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
+
+class ShareViewModel2 : MoleculeViewModel<ShareEvent, ShareState>() {
+
+    override val initialState: ShareState = ShareState()
+
+    @Composable
+    override fun models(events: Flow<ShareEvent>): ShareState {
+        return shareModel(initialState, events)
+    }
+}
+
+@Composable
+fun shareModel(
+    initialState: ShareState,
+    events: Flow<ShareEvent>,
+    store: AppStore = koinInject()
+): ShareState {
+    // TODO: Continue
+
+    return initialState
+}
+data class ShareState(
+    val query: String? = null,
+    val tasks: List<TaskUiModel> = listOf(),
+    val searchResults: List<TaskUiModel> = listOf()
+)
+
+sealed class ShareEvent {
+
+}
 
 class ShareViewModel(
     getActiveTasksFlowUseCase: GetActiveTasksFlowUseCase,
