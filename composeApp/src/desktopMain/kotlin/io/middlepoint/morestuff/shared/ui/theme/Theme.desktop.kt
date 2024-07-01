bb94698cc038d852a49e545cdb32464b78572e89
Joe3112 <jhsagiv@gmail.com>
2024-07-01 14:37:46 +0200

    Add desktop theme

--- composeApp/src/desktopMain/kotlin/io/middlepoint/morestuff/shared/ui/theme/Theme.desktop.kt
+++ composeApp/src/desktopMain/kotlin/io/middlepoint/morestuff/shared/ui/theme/Theme.desktop.kt
@@ -1,8 +1,27 @@
 package io.middlepoint.morestuff.shared.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import io.middlepoint.morestuff.shared.domain.enums.AppTheme
+import io.middlepoint.morestuff.shared.ui.local.LocalTheme
 
 @Composable
 actual fun MoreStuffTheme(content: @Composable () -> Unit) {
+  val isDarkTheme = when (LocalTheme.current) {
+    AppTheme.System -> isSystemInDarkTheme()
+    AppTheme.Light -> false
+    AppTheme.Dark -> true
+  }
 
+  val colorScheme = when {
+    isDarkTheme -> DarkColors
+    else -> LightColors
+  }
+
+  MaterialTheme(
+    colorScheme = colorScheme
+  ) {
+    content()
+  }
 }
