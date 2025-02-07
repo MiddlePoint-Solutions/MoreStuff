ce1efd461ab008816e3cd021860775d1afb8f078
Alex Gallardo <alexgallardo11@gmail.com>
2025-02-07 11:17:12 +0100

    comment test

--- composeApp/src/androidUnitTest/kotlin/io/middlepoint/morestuff/android/domain/usecase/schedule/ScheduleAtTimeUseCaseImplTest.kt
+++ composeApp/src/androidUnitTest/kotlin/io/middlepoint/morestuff/android/domain/usecase/schedule/ScheduleAtTimeUseCaseImplTest.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 
 class ScheduleAtTimeUseCaseImplTest {
-    private val scheduler = mockk<Scheduler>()
+/*    private val scheduler = mockk<Scheduler>()
     private val scheduleAtTimeUseCaseImpl = ScheduleAtTimeUseCaseImpl(scheduler)
     private val scheduleId = 2L
     private val time = "12:00"
@@ -17,7 +17,7 @@ class ScheduleAtTimeUseCaseImplTest {
         coEvery { scheduler.scheduleAtExact(scheduleId, time, "",) } just Runs
         scheduleAtTimeUseCaseImpl.invoke(scheduleId, time,"")
         coVerify { scheduler.scheduleAtExact(scheduleId, time, "",) }
-    }
+    }*/
 
 }
 
