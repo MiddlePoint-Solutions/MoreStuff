c6fce818f9b7cb6a316a592a8563c0008829be28
Alex Gallardo <alexgallardo11@gmail.com>
2025-02-07 12:47:26 +0100

    iOS add back navigation gesture

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,8 +1,13 @@
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.PredictiveBackGestureOverlay
+import com.arkivanov.essenty.backhandler.BackDispatcher
 import io.github.xxfast.decompose.router.LocalRouterContext
 import io.github.xxfast.decompose.router.RouterContext
 import io.middlepoint.morestuff.shared.domain.model.Shareable
@@ -12,6 +17,7 @@ import io.middlepoint.morestuff.shared.ui.App
 import org.koin.compose.koinInject
 
 
+@OptIn(ExperimentalDecomposeApi::class)
 fun MainViewController(routerContext: RouterContext) = ComposeUIViewController {
     CompositionLocalProvider(LocalRouterContext provides routerContext) {
 
@@ -44,7 +50,19 @@ fun MainViewController(routerContext: RouterContext) = ComposeUIViewController {
                 initialScreen.value = screen
             }
         }
-        App(screen = initialScreen.value)
+        PredictiveBackGestureOverlay(
+            backDispatcher = routerContext.backHandler as BackDispatcher,
+            backIcon = { progress, _ ->
+                /*PredictiveBackGestureIcon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    progress = progress,
+                )*/
+            },
+            modifier = Modifier.fillMaxSize(),
+        ){
+            App(screen = initialScreen.value)
+        }
+
     }
 }
 
