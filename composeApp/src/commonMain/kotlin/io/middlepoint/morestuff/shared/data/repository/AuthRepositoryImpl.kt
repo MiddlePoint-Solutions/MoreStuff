4414d0888825e48f17ce97d9a41b04973e7041e1
Joe3112 <jhsagiv@gmail.com>
2025-05-09 15:43:58 +0300

    Reset pull & push times on signout

--- composeApp/src/commonMain/kotlin/io/middlepoint/morestuff/shared/data/repository/AuthRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/io/middlepoint/morestuff/shared/data/repository/AuthRepositoryImpl.kt
@@ -8,18 +8,22 @@ import io.github.jan.supabase.auth.status.SessionStatus
 import io.middlepoint.morestuff.shared.domain.model.AuthFailure
 import io.middlepoint.morestuff.shared.domain.model.Failure
 import io.middlepoint.morestuff.shared.domain.repository.AuthRepository
+import io.middlepoint.morestuff.shared.domain.service.DataSyncManager
 import kotlinx.coroutines.flow.StateFlow
 
 class AuthRepositoryImpl(
   private val supabase: SupabaseClient,
-): AuthRepository {
+  private val syncManager: DataSyncManager,
+) : AuthRepository {
 
   override val authEvents: StateFlow<SessionStatus>
     get() = supabase.auth.sessionStatus
 
   override suspend fun signOut(): Either<Failure, Boolean> = either {
     try {
+      syncManager.push()
       supabase.auth.signOut()
+      syncManager.resetData()
     } catch (e: Exception) {
       raise(AuthFailure(e.message))
     }
