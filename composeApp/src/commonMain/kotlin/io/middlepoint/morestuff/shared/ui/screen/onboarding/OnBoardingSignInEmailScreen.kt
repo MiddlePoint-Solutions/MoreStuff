6cbf5e717b556be1db82103eea7567d0b90cd186
Alex Gallardo <alexgallardo11@gmail.com>
2025-05-15 12:26:52 +0200

    Adjust padding in OnBoardingSignInEmailScreen

--- composeApp/src/commonMain/kotlin/io/middlepoint/morestuff/shared/ui/screen/onboarding/OnBoardingSignInEmailScreen.kt
+++ composeApp/src/commonMain/kotlin/io/middlepoint/morestuff/shared/ui/screen/onboarding/OnBoardingSignInEmailScreen.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
@@ -150,16 +153,20 @@ fun SignInEmailScreen(
     Column(
       modifier = Modifier
         .fillMaxSize()
-        .padding(32.dp),
+        .windowInsetsPadding(WindowInsets.safeDrawing)
+        .padding(horizontal = 32.dp)
+        .padding(bottom = 150.dp),
       horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center
+      verticalArrangement = Arrangement.SpaceBetween
     ) {
+      Spacer(modifier = Modifier.weight(0.4f))
       Icon(
         painter = painterResource(Res.drawable.ms_sign_in),
         tint = Color.Unspecified,
         contentDescription = "signIn",
       )
-      Spacer(modifier = Modifier.height(32.dp))
+      //Spacer(modifier = Modifier.height(32.dp))
+      Spacer(modifier = Modifier.weight(0.6f))
       Text(
         text = if (!isEmailSent) stringResource(Res.string.enter_your_email)
         else stringResource(Res.string.enter_code_sent_to_email, email.text),
