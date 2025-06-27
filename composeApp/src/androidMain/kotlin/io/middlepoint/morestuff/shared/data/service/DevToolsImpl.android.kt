37a83018f9f3cf75122b9a3eee08f5632c79d487
Joe3112 <jhsagiv@gmail.com>
2025-06-27 14:20:02 +0200

    Use application id to get migration file directly from external private app storage

--- composeApp/src/androidMain/kotlin/io/middlepoint/morestuff/shared/data/service/DevToolsImpl.android.kt
+++ composeApp/src/androidMain/kotlin/io/middlepoint/morestuff/shared/data/service/DevToolsImpl.android.kt
@@ -1,6 +1,7 @@
 package io.middlepoint.morestuff.shared.data.service
 
 import io.github.vinceglb.filekit.PlatformFile
+import io.middlepoint.morestuff.shared.MoreStuffApp
 
 actual fun getMigrationDataFile(): PlatformFile? =
-  PlatformFile("/storage/emulated/0/Android/data/io.middlepoint.morestuff.dev/files/files/migration_json.json")
+  PlatformFile("/storage/emulated/0/Android/data/${MoreStuffApp.INSTANCE.packageName}/files/files/migration_json.json")
